147074073d4993153d949546cf839b74c5379c95
Tom <pjoh1092@gmail.com>
2024-06-20 11:15:18 -0500

    initial commit

---
+++ backend/server.js
@@ -0,0 +1,16 @@
+require("dotenv").config();
+
+const express = require("express");
+
+//express app
+const app = express();
+
+//routes
+app.get("/", (req, res) => {
+    res.send({mssg: "Welcom to the app"});
+});
+
+//listen for requestes
+app.listen(process.env.PORT, ()=>{
+    console.log("listening on port", process.env.PORT);
+});
